b0d92ce795ad3c56cec15edfc6af67636686cfd6
Georgii Zhulikov <l8doku@gmail.com>
2022-08-01 11:28:52 +0300

    Add weight parameters

--- TokiPonaNameGenerator.py
+++ TokiPonaNameGenerator.py
@@ -1,10 +1,17 @@
 # toki pona name generator
 from random import choice
+from random import choices
 def generator(amount) -> list[str]:
     # the structure of toki pona syllables is (C)V(N), where C is a consonant, V is a vowel, and N is a nasal sound
     # double consonants and double vowels are forbidden
     # n and m can't be next to each other. in other words, nn, mm, nm, mn are forbidden
     # official words are not used to avoid confusion
+
+    n_syllable_weight = 0.25
+    starts_with_vowel_weight = 5 / 9
+    n_second_letter_weight = 2 / 9
+    break_weight = 2 / 3
+
     consontants = ['p', 't', 'k', 's', 'm', 'n', 'l', 'w', 'j']
     vowels = ['a', 'e', 'i', 'o', 'u']
     forbidden = ['ti', 'ji', 'wo', 'wu']
@@ -15,7 +22,7 @@ def generator(amount) -> list[str]:
         while True:
             syllable = choice(consontants) + choice(vowels) 
             if syllable not in forbidden:
-                if choice([True, False]):
+                if choices([True, False], cum_weights=[n_syllable_weight, 1], k=1)[0]:
                     syllable += 'n'
                 if {ends_in, syllable[0]} not in [{'m', 'n'}, {'n', 'n'}, {'m', 'm'}]:
                     return syllable
@@ -23,17 +30,19 @@ def generator(amount) -> list[str]:
     for _ in range(amount):
         while True: # make sure the name is not a official word or duplicated
             name = ''
-            start_with_vowel = choice([True, False])
+            start_with_vowel = choices(
+                [True, False], cum_weights=[starts_with_vowel_weight, 1], k=1
+            )[0]
             if start_with_vowel:
                 name += choice(vowels)
-                if choice([True, False]):
+                if choices([True, False], cum_weights=[n_second_letter_weight, 1], k=1)[0]:
                     name += 'n'
             else:
                 name += random_syllable()
             # determine how many syllables are in the name
             while True:
                 name += random_syllable(name[-1])
-                if choice([True, True, False]):
+                if choices([True, False], cum_weights=[break_weight, 1], k=1)[0]:
                     break
             
             if name not in words and name.capitalize() not in names:
